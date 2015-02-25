a4ac855d6c663e5edf69d48a524bbe2ba9e9cbdc
Adrinus <mckibbon@gmail.com>
2015-02-25 11:24:46 -0500

    Cleanup and Scanner Implementation
    Switched to Scanner for file parsing. Added more names, added a Server
    Pseudo Code Doc.

--- src/util/NameGenerator.java
+++ src/util/NameGenerator.java
@@ -1,49 +1,39 @@
 package util;
 
 import java.io.File;
-import java.io.IOException;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
-import com.google.common.base.Charsets;
-import com.google.common.io.Files;
-
 public class NameGenerator {
-	private File firstFile = new File("src/util/Names/first.txt");
-	private File lastFile = new File("src/util/Names/last.txt");
-	private File prefixFile = new File("src/util/Names/prefix.txt");
-	private File suffixFile = new File("src/util/Names/suffix.txt");
 	private String name = "";
 	private List<String> first = new ArrayList<String>();
 	private List<String> last = new ArrayList<String>();
 	private List<String> prefix = new ArrayList<String>();
 	private List<String> suffix = new ArrayList<String>();
-	public String NameGenerator(String kind){
-		try {
-			ArrayList<String> first = (ArrayList<String>) Files.readLines(firstFile, Charsets.UTF_8);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	public String NameGenerator(String kind) throws FileNotFoundException{
+		Scanner firstNames = new Scanner(new File("src/util/Names/first.txt"));
+		while(firstNames.hasNext()){
+			first.add(firstNames.next());
 		}
-		try {
-			ArrayList<String> last = (ArrayList<String>) Files.readLines(lastFile, Charsets.UTF_8);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		firstNames.close();
+		Scanner lastNames = new Scanner(new File("src/util/Names/last.txt"));
+		while(lastNames.hasNext()){
+			last.add(lastNames.next());
 		}
-		try {
-			ArrayList<String> prefix = (ArrayList<String>) Files.readLines(prefixFile, Charsets.UTF_8);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		lastNames.close();
+		Scanner prefixes = new Scanner(new File("src/util/Names/prefix.txt"));
+		while(prefixes.hasNext()){
+			prefix.add(prefixes.next());
 		}
-		try {
-			ArrayList<String> suffix = (ArrayList<String>) Files.readLines(suffixFile, Charsets.UTF_8);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		prefixes.close();
+		Scanner suffixes = new Scanner(new File("src/util/Names/suffix.txt"));
+		while(suffixes.hasNext()){
+			suffix.add(suffixes.next());
 		}
+		suffixes.close();
 		if(kind.equals("person")){
 			name = person();
 		}
